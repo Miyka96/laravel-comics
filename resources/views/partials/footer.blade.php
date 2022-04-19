0e85aed7cee71d2c4404541f274a1c884f7e6be5
Miyka96 <micaelaseminara@outlook.it>
2022-04-20 01:55:49 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,28 @@
-footer
+@php
+$footerLinks = array(
+
+        "DC COMICS"=> array('Characters','Comics','Tv','Games','Videos','News'),
+        'DC'=>array('Terms Of Use', 'Privacy policy (New)', 'Ad Choices', 'Advertising', 'Jobs', 'Subscriptions', 'Talent Workshops', 'CPSC Certificates', 'Ratings', 'Shop Help', 'Contact Us'),
+        'SITES'=>array('DC', 'MAD Magazine', 'DC Kids', 'DC Universe', 'DC Power Visa'),
+        'SHOP'=>array('Shop DC', 'Shop DC Collectibles')
+);
+@endphp
+
+<footer class="container-fluid">
+    <div class="container">
+        <ul>
+            @foreach ($footerLinks as $key => $val)
+                <li>
+                    <div id="title">{{ $key }}</div>
+                    @foreach ($val as $element) 
+                    <div>
+                        {{ $element }}
+                    </div>
+                    @endforeach
+                </li>
+            @endforeach
+        </ul>
+
+        <figure></figure>
+    </div>
+</footer>
